22b0277bc5d4b26b0b9c69a52ea32cb9b508c76d
Admin <1234@qq.com>
2022-04-06 10:33:26 +0800

    api array stream

--- src/main/java/com/example/demo/skill/apimethod/array/ArrayFilterTest.java
+++ src/main/java/com/example/demo/skill/apimethod/array/ArrayFilterTest.java
@@ -1,9 +1,13 @@
 package com.example.demo.skill.apimethod.array;
 
+import com.example.demo.entity.Person;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author ldy
@@ -12,10 +16,32 @@ import java.util.Objects;
 public class ArrayFilterTest {
 
     public static void main(String[] args) {
-        List<Integer> arrayList = new ArrayList<>(2);
-        arrayList.add(2);
-        arrayList.add(5);
-        boolean b = Arrays.stream(new int[arrayList.size()]).anyMatch(2);
+        List<Person> arrayList = new ArrayList<>(2);
+        arrayList.add(new Person(2));
+        arrayList.add(new Person(5));
+        /** 返回值Boolean，可用于判断 */
+        if(Arrays.stream(arrayList.toArray(new Person[arrayList.size()])).anyMatch(p -> Objects.equals(2,p.getSex()))){
+            System.out.println("ggggg");
+        }
+
+        /** 统计年龄为2 的数量 */
+        long count = Arrays.stream(arrayList.toArray(new Person[arrayList.size()])).filter(p ->  Objects.equals(2,p.getSex())).count();
+        System.out.println(count);
+
+        /** 获取集合中第一个元素，如果集合为空，则为null */
+        Person person = Arrays.stream(arrayList.toArray(new Person[arrayList.size()])).filter(p -> Objects.equals(2, p.getSex())).findFirst().orElse(null);
+        System.out.println(person);
+
+        /** 筛选年龄为2的人的list */
+        List<Person> personList = Arrays.stream(arrayList.toArray(new Person[arrayList.size()])).filter(p -> Objects.equals(2, p.getSex())).collect(Collectors.toList());
+        System.out.println(personList.toString());
+
+        Stream<Person> limit = Arrays.stream(arrayList.toArray(new Person[arrayList.size()])).filter(p -> Objects.equals(2, p.getSex())).limit(0);
+        // limit.getClass() 输出 class java.util.stream.SliceOps$1
+        System.out.println(limit.getClass());
+        // limit.getClass().getName() 输出 java.util.stream.SliceOps$1
+        System.out.println(limit.getClass().getName());
+
 
     }
 }
--- src/main/java/com/example/demo/skill/apimethod/list/filter/ListFilterTest.java
+++ src/main/java/com/example/demo/skill/apimethod/list/filter/ListFilterTest.java
@@ -19,20 +19,27 @@ public class ListFilterTest {
         // 获取集合中第一个元素，如果集合为空，则为null
         Person person1 = personArrayList1.stream().findFirst().orElse(null);
         System.out.println(person1);
-        
-        Person person2 = personArrayList.stream().findFirst().orElse(null);// 获取集合中第一个元素，如果集合为空，则为null
+
+        /** 获取集合中第一个元素，如果集合为空，则为null */
+        Person person2 = personArrayList.stream().findFirst().orElse(null);
         System.out.println(person2);
 
         if(!CollectionUtils.isEmpty(personArrayList)){
-            personArrayList = personArrayList.stream().filter(person -> Objects.equals(1, person.getSex())).collect(Collectors.toList());// 筛选年龄为1的人
+            /** 筛选年龄为1的人的list */
+            personArrayList = personArrayList.stream().filter(person -> Objects.equals(1, person.getSex())).collect(Collectors.toList());
+            /** 指定条件筛选判断 */
+            boolean b = personArrayList.stream().anyMatch(p -> Objects.equals(1, p.getSex()));
+            System.out.println(b);
         }
 
-        Optional<Person> any = personArrayList.stream().filter(person -> Objects.equals(1, person.getSex())).findAny();// 指定条件筛选判断
+        /** 指定条件筛选判断 */
+        Optional<Person> any = personArrayList.stream().filter(person -> Objects.equals(1, person.getSex())).findAny();
         if (any.isPresent()) {
             System.out.println("存在年龄为1的人");
         }
 
-        List<Person> personList = any.map(Collections::singletonList).orElse(Collections.emptyList());// Optional转换成list
+        /** Optional转换成list */
+        List<Person> personList = any.map(Collections::singletonList).orElse(Collections.emptyList());
 
         System.out.println(personArrayList);
     }
