ae65a1c1234a0f5bf54dc435e9b444a13fb513a9
dyli <dyli@163.com>
2022-04-07 16:18:11 +0800

    annotation

--- src/main/java/com/example/demo/skill/annotationcustom/User.java
+++ src/main/java/com/example/demo/skill/annotationcustom/User.java
@@ -3,23 +3,20 @@ package com.example.demo.skill.annotationcustom;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
+    @NotNull
     private String userName;
+
     @AnnotationCustom
     private String phoneNumber;
 
-    public static void main(String[] args) {
-
-        try {
-            User user = new User("哈哈哈","aaa");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    // controller的各个方法入参User处添加 @Valid注解 import javax.validation.Valid ， public RespBean doLogin(@Valid LoginVo loginVo) {}
 
 }
