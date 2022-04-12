e8e96339ca3f823f22f1e36c20f6407a6448b71c
dyli <dyli@163.com>
2022-04-12 11:29:00 +0800

    GlobalException

---
+++ src/main/java/com/example/demo/skill/apimethod/exception/globalexceptionhandler/GlobalException.java
@@ -0,0 +1,23 @@
+package com.example.demo.skill.apimethod.exception.globalexceptionhandler;
+
+/**
+ * @author ldy
+ * @version 1.0
+ */
+
+public class GlobalException extends RuntimeException {
+
+    private RespBeanEnum respBeanEnum;
+
+    public RespBeanEnum getRespBeanEnum() {
+        return respBeanEnum;
+    }
+
+    public void setRespBeanEnum(RespBeanEnum respBeanEnum) {
+        this.respBeanEnum = respBeanEnum;
+    }
+
+    public GlobalException(RespBeanEnum respBeanEnum) {
+        this.respBeanEnum = respBeanEnum;
+    }
+}
