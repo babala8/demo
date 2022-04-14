7f73d14640a19c3faff7941e6eb14662473a0f1a
Admin <1234@qq.com>
2022-04-14 17:01:20 +0800

    thread

--- src/main/java/com/example/demo/skill/thread/CustomThreadPool.java
+++ src/main/java/com/example/demo/skill/thread/CustomThreadPool.java
@@ -1,5 +1,42 @@
 package com.example.demo.skill.thread;
 
+import java.util.concurrent.*;
+
+
 public class CustomThreadPool {
+
+    private static final int CORE_POOL_SIZE = 10;
+    private static final int MAXINUM_POOL_SIZE = 20;
+    private static final long KEEP_ALIVE_TIME = 60;
+    private static final int MAXINUM_QUEUE_SIZE = 100;
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                CORE_POOL_SIZE,
+                MAXINUM_POOL_SIZE,
+                KEEP_ALIVE_TIME,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(MAXINUM_QUEUE_SIZE),
+                Executors.defaultThreadFactory(),
+//                new ThreadPoolExecutor.AbortPolicy()// 拒绝策略
+                new CustomRejectedExecutionHandler()
+                );
+    }
+
+
+    private static class CustomRejectedExecutionHandler implements RejectedExecutionHandler{
+
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            BlockingQueue<Runnable> queue = executor.getQueue();
+            try {
+                queue.put(r);
+            } catch (InterruptedException e) {
+                System.out.println("拒绝策略重新提交任务失败");
+                e.printStackTrace();
+            }
+
+        }
+    }
 
 }
